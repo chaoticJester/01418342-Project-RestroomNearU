687a4bcc87bc435cab16a6fa0eee0b709f6979d4
Pheeraphat Jumnong <pheeraphat.j@ku.th>
2026-02-03 22:28:15 +0700

    Refactor: Move add_new_restroom_page to pages directory
    - lib/add_new_restroom_page.dart: Deleted from lib root
    - lib/pages/add_new_restroom_page.dart: Moved to pages directory to follow project structure convention
    - lib/main.dart: Updated import path from 'restroom_near_u/add_new_restroom_page.dart' to 'restroom_near_u/pages/add_new_restroom_page.dart' to reflect new file location

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:restroom_near_u/login_page.dart';
-import 'package:restroom_near_u/home_page.dart';
-import 'package:restroom_near_u/add_new_restroom_page.dart';
+import 'package:restroom_near_u/pages/login_page.dart';
+import 'package:restroom_near_u/pages/user_homepage.dart';
+import 'package:restroom_near_u/pages/add_new_restroom_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/login_page': (context) => LoginPage(),
-        '/home_page' : (context) => HomePage(),
+        '/home_page' : (context) => UserHomePage(),
         '/add_new_restroom': (context) => const AddNewRestroomPage(),
       }, 
       home: UserHomePage(),
